d7a5d8b9a962d16a395ed2aacbbb2b9ad071f382
bishusom <bishusom@gmail.com>
2025-12-06 14:58:29 +0800

    Moving to next 16.0.7

--- next.config.js
+++ next.config.js
@@ -8,10 +8,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: false,
   },
-  // Skip ESLint during build
-  eslint: {
-    ignoreDuringBuilds: true,
-  }
 }
 
 module.exports = nextConfig
